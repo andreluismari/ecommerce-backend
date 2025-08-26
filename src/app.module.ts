ad997894b7baaa4ab3cc76fd7dfe6a82e96547a7
Andre Luis Mari <andreluismari@gmail.com>
2025-08-25 22:04:39 -0300

    Aula 25/08/2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,28 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from './cases/categories/category.module';
 import { BrandModule } from './cases/brands/brand.module';
+import { ProductModule } from './cases/products/product.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db.voplxfqkgtorzyssxzzl.supabase.co',
-      port: +'5432',
-      username: 'postgres',
-      password: 'Unimater123@!',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
-      ssl: { rejectUnauthorized: false }, // Supabase
     }),
     CategoryModule,
     BrandModule,
+    ProductModule
   ],
 })
 export class AppModule {}
